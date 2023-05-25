0711e8366c22e14bb9a6d622b7fdbe9423a26887
Steve Higham (MSI Ubuntu) <steve@sjlt.co.uk>
2023-05-25 14:35:59 +0100

    .gitignore updated.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const title = React.createElement("h1", null, "Hello world!");
-const domElement = document.getElementById("root");
-ReactDOM.createRoot(domElement).render(title);
+const title =
+  React.createElement ("h1", null, "Hello Steve!");
+const domElement = document.getElementById ("root");
+ReactDOM.createRoot (domElement).render (title);
