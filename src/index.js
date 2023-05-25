dd20f8205500dd569776f2985f1a03d8a86a70b7
Steve Higham (MSI Ubuntu) <steve@sjlt.co.uk>
2023-05-25 17:37:49 +0100

    This is rq02-nesting-italic.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const world =
+  React.createElement ('em', null, 'world');
 const title =
-  React.createElement ("h1", null, "Hello Steve!");
+  React.createElement ("h1", null, "Hello ", world, '!');
 const domElement = document.getElementById ("root");
 ReactDOM.createRoot (domElement).render (title);
