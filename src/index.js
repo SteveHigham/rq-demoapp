c226f23e903fe3d1da49ef333ac87224c2ce6840
Steve Higham (iMac) <steve@sjlt.co.uk>
2023-05-29 19:26:27 +0100

    Completed to rq02-links-app.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import {createRoot} from "react-dom/client";
+import App from './App';
 
-class Link extends React.Component
-{
-  render ()
-  {
-    return React.createElement (
-      'p',
-      null,
-      React.createElement ('a', {href: this.props.url}, this.props.children)
-    );
-  }
-}
-
-const boldReact =
-  React.createElement ('strong', null, 'React');
-
-const link1 =
-  React.createElement (Link, {url: '//react.dev'}, boldReact);
-const link2 =
-  React.createElement (Link, {url: '//vuejs.org'}, 'Vue');
-const link3 =
-  React.createElement (Link, {url: '//angular.io'}, 'Angular');
-const group =
-  React.createElement (React.Fragment, null, link1, link2, link3);
-const domElement = document.getElementById ("root");
-ReactDOM.createRoot (domElement).render (group);
+createRoot (document.getElementById ("root")).render (React.createElement (App));
 
 // End of index.js
